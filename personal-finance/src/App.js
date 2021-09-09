9951ec732ca30ba29b4ab4004d6896b32e609656
Higor Coimbra <higor.coimbra@dtidigital.com.br>
2021-09-08 21:40:51 -0300

    incluing eslint and prettier

--- personal-finance/src/App.js
+++ personal-finance/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Fragment } from 'react/cjs/react.production.min';
 
 function App() {
-  return (
-    <>
-    </>
-  );
+    const a = 'teste';
+    if (true) {
+        useState();
+    }
+
+    return (
+        <Fragment>
+            <div>teste</div>
+        </Fragment>
+    );
 }
 
 export default App;
